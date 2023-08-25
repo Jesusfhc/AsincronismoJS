00fe9c5154459d5994bf391a58aaac7657d072bb
Jesus Hernandez <jjesusfhernandezc@gmail.com>
2023-08-24 20:29:44 -0400

    terminado curso de asincronismo

--- src/async/challange.js
+++ src/async/challange.js
@@ -3,7 +3,7 @@ const API = 'https://api.escuelajs.co/api/v1';
 
 async function fetchData(urlAPI) {
     const response = await fetch(urlAPI);
-    const data = await response.json()
+    const data = await response.json();
     return data;
 }
 
@@ -11,8 +11,16 @@ const anotherFn = async (urlAPI) => {
     try {
         const products = await fetchData(`${urlAPI}/products`);
         const product = await fetchData(`${urlAPI}/products/${products[0].id}`);
-        
-    } catch {
+        const category = await fetchData(`${urlAPI}/categories/${product.gategory.id}`);
 
+        console.log(products);
+        console.log(product.title);
+        console.log(category.name);
+        
+    } catch (error) {
+        console.error(error);
     }
 }
+
+anotherFn(API);
+
---
+++ src/generator/index.js
@@ -0,0 +1,11 @@
+function* iterate(array) { //reserve word function* al hacer esto iniciamos un generador
+    for (let value of array) {
+        yield value // reserve word para generators
+    }
+}
+
+const it = iterate(['oscar', 'david', 'ulices', 'jennifer', 'ana']);
+
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
